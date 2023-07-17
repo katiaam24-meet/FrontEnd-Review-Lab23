b43949ab47012180870739ee15f35f5a2481e38d
katiaam24-meet <139688886+katiaam24-meet@users.noreply.github.com>
2023-07-17 18:02:17 +0300

    Commit

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-function changeBackgroundColor(color){
-	document.getElementById("body").style.backgroundColor = color;
+function changeBackgroundColor(color1, color2){
+	let body = document.getElementById("body");
+	if(body.style.backgroundColor != color1){
+		body.style.backgroundColor = color1
+	}
+	else
+	{
+		body.style.backgroundColor = color2
+		
+	}
 } 
+	
+
+
+  
